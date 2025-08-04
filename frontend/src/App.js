751000a5cfed5663c6bab875d104c3c64d87232e
anmol37082 <singanmol714@gmail.com>
2025-08-04 16:41:11 +0530

    Your commit message here

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -21,19 +20,82 @@ import Header from './components/Header';
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/order/:id" element={<OrderPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<UserLogin />} />
-        <Route path="/my-orders" element={<MyOrders />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/admin" element={<AdminLogin />} />
+        {/* Public Routes with Header */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/menu"
+          element={
+            <>
+              <Header />
+              <Menu />
+            </>
+          }
+        />
+        <Route
+          path="/order/:id"
+          element={
+            <>
+              <Header />
+              <OrderPage />
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <Header />
+              <UserLogin />
+            </>
+          }
+        />
+        <Route
+          path="/my-orders"
+          element={
+            <>
+              <Header />
+              <MyOrders />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <Header />
+              <ProfilePage />
+            </>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <>
+              <Header />
+              <AdminLogin />
+            </>
+          }
+        />
 
-        {/* Admin Routes under layout */}
+        {/* Admin Panel with Sidebar (NO public Header) */}
         <Route
           path="/adminpanel"
           element={
