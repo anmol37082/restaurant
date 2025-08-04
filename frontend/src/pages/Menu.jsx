065ad00f18148edb74b276b2658d164471f6ccc3
anmol37082 <singanmol714@gmail.com>
2025-08-04 13:59:15 +0530

    Your commit message here

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import styles from '../Menu.module.css';
+import styles from './Menu.module.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
