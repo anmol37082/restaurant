9a765ae2123e7751b8cec6a9328fe89d887a91f3
anmol37082 <singanmol714@gmail.com>
2025-08-05 11:35:40 +0530

    Your commit message here

--- frontend/src/AdminPanel.jsx
+++ frontend/src/AdminPanel.jsx
@@ -7,15 +7,19 @@ import OrderList from './pages/OrderList';
 const AdminPanel = () => {
   return (
     <div className="d-flex">
+      {/* Sidebar */}
       <AdminSidebar />
+      {/* Main Content */}
       <div className="flex-grow-1 p-4" style={{ marginLeft: '220px' }}>
         <Routes>
-          {/* ✅ Default route: redirects /admin to /admin/add-menu */}
-          <Route path="/" element={<Navigate to="add-menu" />} />
-          
-          {/* ✅ Other admin routes */}
+          {/* Redirect /admin to /admin/add-menu */}
+          <Route index element={<Navigate to="add-menu" replace />} />
+          {/* Add Menu Page */}
           <Route path="add-menu" element={<AddMenu />} />
+          {/* Orders Page */}
           <Route path="orders" element={<OrderList />} />
+          {/* Catch-all: Redirect unknown admin routes to add-menu */}
+          <Route path="*" element={<Navigate to="add-menu" replace />} />
         </Routes>
       </div>
     </div>
